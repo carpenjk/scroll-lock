910a442a059d90fa86f256c63e9aab4f6a3ac59a
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-08-08 15:26:57 -0400

    feat: initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-export { default as MyComponent } from './components/MyComponent'
+import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'
+import useIsoLayoutEffect from '../hooks/UseIsoLayoutEffect'
+
+const ScrollLock = ({ scrollNode, reserveScrollBarGap }) => {
+  const options = {
+    reserveScrollBarGap
+  }
+  function clear () {
+    clearAllBodyScrollLocks()
+  }
+  useIsoLayoutEffect(() => {
+    disableBodyScroll(scrollNode, options)
+    return clear
+  }, [])
+  return null
+}
+
+export default ScrollLock
